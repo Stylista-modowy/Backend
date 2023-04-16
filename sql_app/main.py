962f9678d335e17d3093bf6853f00cf719690e05
LucasBywalec <lukasz.bywalec@outlook.com>
2023-04-16 14:04:01 +0200

    init + signup and signin working

---
+++ sql_app/main.py
@@ -0,0 +1,59 @@
+from typing import List, Annotated
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas, database, security
+from .database import SessionLocal, engine
+
+from datetime import timedelta
+
+from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from fastapi.middleware.cors import CORSMiddleware
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/auth/signup/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.post("/auth/signin/", response_model=schemas.Token)
+async def login_for_access_token(
+    # form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    # db: Session = Depends(database.get_db)
+    form_data: schemas.UserLogin, db: Session = Depends(database.get_db)
+):
+    print(form_data)
+    user = security.authenticate_user(
+        db=db, username=form_data.username, password=form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(
+        minutes=security.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = security.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
---
+++ sql_app/schemas.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class User(UserBase):
+    username: str
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
