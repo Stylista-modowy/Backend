a7210381f4a7960c0a00db3723bf3bf7fa4a0ff1
LucasBywalec <lukasz.bywalec@outlook.com>
2023-06-19 18:51:29 +0200

    no rembg

--- sql_app/image_crop.py
+++ sql_app/image_crop.py
@@ -21,7 +21,6 @@ class Gender(Enum):
 	
 
 def trim(im):
-    im = Image.open(im)
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
     diff = ImageChops.difference(im, bg)
     diff = ImageChops.add(diff, diff, 2.0, -100)
