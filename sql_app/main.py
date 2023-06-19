6922039e407e0ef858383eab6d06f036b2ffba20
LucasBywalec <lukasz.bywalec@outlook.com>
2023-06-19 20:15:55 +0200

    form change

--- sql_app/main.py
+++ sql_app/main.py
@@ -81,36 +81,36 @@ async def login_for_access_token(
 #     return
 
 @app.post("/wardrobe/add/")
-async def add_items_to_wardrobe(token: str, items: List[schemas.Item] = Form(), db: Session = Depends(database.get_db)):
+async def add_items_to_wardrobe(token: str, item: schemas.Item = Form(), db: Session = Depends(database.get_db)):
     # print(f'ITEMS: {len(items)}')
     decoded_token = security.read_id_from_token(token=token)
     print(token)
-    for item in items:
-        print(item)
-        numbers = [int(x) for x in item.item_image.decode('utf-8').split(",")]
-        byte_array = bytearray(numbers)
-        image = Image.open(io.BytesIO(byte_array))
+    # for item in items:
+    print(item)
+    numbers = [int(x) for x in item.item_image.decode('utf-8').split(",")]
+    byte_array = bytearray(numbers)
+    image = Image.open(io.BytesIO(byte_array))
 
-        print(image)
-        print("\n\n\n")
-        image = trim(image)
-        print(image)
-        print("\n\n\n")
+    print(image)
+    print("\n\n\n")
+    image = trim(image)
+    print(image)
+    print("\n\n\n")
 
-        byte_array2 = io.BytesIO()
-        print(byte_array2)
-        print("\n\n\n")
-        image.save(byte_array2, format='PNG')
-        print(image)
-        print("\n\n\n")
-        byte_array2 = byte_array2.getvalue()
-        numbers2 = ",".join(str(x) for x in byte_array2)
-        print(numbers2)
-        print("\n\n\n")
-        item.item_image = numbers2
-        print(item)
-        print("\n\n\n")
-        crud.create_item(db=db, item=item, id=decoded_token)
+    byte_array2 = io.BytesIO()
+    print(byte_array2)
+    print("\n\n\n")
+    image.save(byte_array2, format='PNG')
+    print(image)
+    print("\n\n\n")
+    byte_array2 = byte_array2.getvalue()
+    numbers2 = ",".join(str(x) for x in byte_array2)
+    print(numbers2)
+    print("\n\n\n")
+    item.item_image = numbers2
+    print(item)
+    print("\n\n\n")
+    crud.create_item(db=db, item=item, id=decoded_token)
     return
 
 # def convert_image_to_bytes(image):
